66870531cad3c8ee72e0fb4ddb526e71d7c12f71
jgoletto <julien.goletto@ose-engineering.fr>
2024-03-14 19:06:51 +0100

    :sparkles: Basic form behaviour

---
+++ src/app/RegistrationForm.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registrationSchema } from "./registrationSchema";
+import type { Registration } from "./registrationSchema";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@ui/form";
+import { Button } from "@ui/button";
+import { Input } from "@ui/input";
+
+export const RegistrationForm = () => {
+  const form = useForm<Registration>({
+    resolver: zodResolver(registrationSchema),
+    defaultValues: {
+      first: "",
+      last: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = (data: Registration) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+        <fieldset className="grid grid-cols-2 gap-8">
+          <FormField
+            control={form.control}
+            name="first"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel htmlFor="first">First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormDescription>Your first name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="last"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel htmlFor="last">Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormDescription>Your last name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormDescription>Your email address.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </fieldset>
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
-
+import { RegistrationForm } from "./RegistrationForm";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <RegistrationForm />
+    </main>
   );
 }
